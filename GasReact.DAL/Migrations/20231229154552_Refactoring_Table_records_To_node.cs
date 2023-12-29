60cc167d644e5bffd4fabbd43b6e9aaf2114cc80
mk2b79 <samchuk.maks@outlook.com>
2023-12-29 18:11:20 +0200

    I just forgot to dot the migration

---
+++ GasReact.DAL/Migrations/20231229154552_Refactoring_Table_records_To_node.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GasReact.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Refactoring_Table_records_To_node : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_indicators_records_record_id",
+                schema: "public",
+                table: "indicators");
+
+            migrationBuilder.DropTable(
+                name: "records",
+                schema: "public");
+
+            migrationBuilder.CreateTable(
+                name: "node",
+                schema: "public",
+                columns: table => new
+                {
+                    record_id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    fermentator_id = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_node", x => x.record_id);
+                    table.ForeignKey(
+                        name: "FK_node_fermentators_fermentator_id",
+                        column: x => x.fermentator_id,
+                        principalSchema: "public",
+                        principalTable: "fermentators",
+                        principalColumn: "fermentator_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_node_fermentator_id",
+                schema: "public",
+                table: "node",
+                column: "fermentator_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_indicators_node_record_id",
+                schema: "public",
+                table: "indicators",
+                column: "record_id",
+                principalSchema: "public",
+                principalTable: "node",
+                principalColumn: "record_id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_indicators_node_record_id",
+                schema: "public",
+                table: "indicators");
+
+            migrationBuilder.DropTable(
+                name: "node",
+                schema: "public");
+
+            migrationBuilder.CreateTable(
+                name: "records",
+                schema: "public",
+                columns: table => new
+                {
+                    record_id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    fermentator_id = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_records", x => x.record_id);
+                    table.ForeignKey(
+                        name: "FK_records_fermentators_fermentator_id",
+                        column: x => x.fermentator_id,
+                        principalSchema: "public",
+                        principalTable: "fermentators",
+                        principalColumn: "fermentator_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_records_fermentator_id",
+                schema: "public",
+                table: "records",
+                column: "fermentator_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_indicators_records_record_id",
+                schema: "public",
+                table: "indicators",
+                column: "record_id",
+                principalSchema: "public",
+                principalTable: "records",
+                principalColumn: "record_id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ GasReact.DAL/Migrations/20231229154854_Refactoring_Table_node_To_nodes.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GasReact.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Refactoring_Table_node_To_nodes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_indicators_node_record_id",
+                schema: "public",
+                table: "indicators");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_node_fermentators_fermentator_id",
+                schema: "public",
+                table: "node");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_node",
+                schema: "public",
+                table: "node");
+
+            migrationBuilder.RenameTable(
+                name: "node",
+                schema: "public",
+                newName: "nodes",
+                newSchema: "public");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_node_fermentator_id",
+                schema: "public",
+                table: "nodes",
+                newName: "IX_nodes_fermentator_id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_nodes",
+                schema: "public",
+                table: "nodes",
+                column: "record_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_indicators_nodes_record_id",
+                schema: "public",
+                table: "indicators",
+                column: "record_id",
+                principalSchema: "public",
+                principalTable: "nodes",
+                principalColumn: "record_id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_nodes_fermentators_fermentator_id",
+                schema: "public",
+                table: "nodes",
+                column: "fermentator_id",
+                principalSchema: "public",
+                principalTable: "fermentators",
+                principalColumn: "fermentator_id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_indicators_nodes_record_id",
+                schema: "public",
+                table: "indicators");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_nodes_fermentators_fermentator_id",
+                schema: "public",
+                table: "nodes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_nodes",
+                schema: "public",
+                table: "nodes");
+
+            migrationBuilder.RenameTable(
+                name: "nodes",
+                schema: "public",
+                newName: "node",
+                newSchema: "public");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_nodes_fermentator_id",
+                schema: "public",
+                table: "node",
+                newName: "IX_node_fermentator_id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_node",
+                schema: "public",
+                table: "node",
+                column: "record_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_indicators_node_record_id",
+                schema: "public",
+                table: "indicators",
+                column: "record_id",
+                principalSchema: "public",
+                principalTable: "node",
+                principalColumn: "record_id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_node_fermentators_fermentator_id",
+                schema: "public",
+                table: "node",
+                column: "fermentator_id",
+                principalSchema: "public",
+                principalTable: "fermentators",
+                principalColumn: "fermentator_id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
