8a4b7e114c6ca2518272457304bfdd0558c67823
mk2b79 <samchuk.maks@outlook.com>
2024-01-03 14:01:13 +0200

    Use ChartService

--- GasReact.WebApp/Controllers/ChartController.cs
+++ GasReact.WebApp/Controllers/ChartController.cs
@@ -1,46 +1,20 @@
 using GasReact.BLL.DTO;
+using GasReact.BLL.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GasReact.WebApp.Controllers;
 
 public class ChartController : Controller
 {
+    private ChartServices _chartServices;
+    public ChartController(ChartServices chartServices)
+    {
+        _chartServices = chartServices;
+    }
     public IActionResult Index()
     {
-        List<IndicatorChartDto> records = new List<IndicatorChartDto>
-        {
-            new IndicatorChartDto { Value = 10, Date = new DateTime(2023, 1, 1) },
-            new IndicatorChartDto { Value = 25, Date = new DateTime(2023, 1, 2) },
-            new IndicatorChartDto { Value = 13, Date = new DateTime(2023, 1, 3) },
-            new IndicatorChartDto { Value = 32, Date = new DateTime(2023, 1, 4) },
-            new IndicatorChartDto { Value = 20, Date = new DateTime(2023, 1, 5) },
-            new IndicatorChartDto { Value = 10, Date = new DateTime(2023, 1, 1) },
-            new IndicatorChartDto { Value = 10, Date = new DateTime(2023, 1, 1) },
-            new IndicatorChartDto { Value = 25, Date = new DateTime(2023, 1, 2) },
-            new IndicatorChartDto { Value = 13, Date = new DateTime(2023, 1, 3) },
-            new IndicatorChartDto { Value = 32, Date = new DateTime(2023, 1, 4) },
-            new IndicatorChartDto { Value = 20, Date = new DateTime(2023, 1, 5) },
-            new IndicatorChartDto { Value = 10, Date = new DateTime(2023, 1, 1) },
-            new IndicatorChartDto { Value = 25, Date = new DateTime(2023, 1, 2) },
-            new IndicatorChartDto { Value = 13, Date = new DateTime(2023, 1, 3) },
-            new IndicatorChartDto { Value = 32, Date = new DateTime(2023, 1, 4) },
-            new IndicatorChartDto { Value = 20, Date = new DateTime(2023, 1, 5) },
-            new IndicatorChartDto { Value = 10, Date = new DateTime(2023, 1, 1) },
-            new IndicatorChartDto { Value = 25, Date = new DateTime(2023, 1, 2) },
-            new IndicatorChartDto { Value = 13, Date = new DateTime(2023, 1, 3) },
-            new IndicatorChartDto { Value = 32, Date = new DateTime(2023, 1, 4) },
-            new IndicatorChartDto { Value = 10, Date = new DateTime(2023, 1, 1) },
-            new IndicatorChartDto { Value = 25, Date = new DateTime(2023, 1, 2) },
-            new IndicatorChartDto { Value = 13, Date = new DateTime(2023, 1, 3) },
-            new IndicatorChartDto { Value = 32, Date = new DateTime(2023, 1, 4) },
-            new IndicatorChartDto { Value = 20, Date = new DateTime(2023, 1, 5) },
-            new IndicatorChartDto { Value = 10, Date = new DateTime(2023, 1, 1) },
-            new IndicatorChartDto { Value = 25, Date = new DateTime(2023, 1, 2) },
-            new IndicatorChartDto { Value = 13, Date = new DateTime(2023, 1, 3) },
-            new IndicatorChartDto { Value = 32, Date = new DateTime(2023, 1, 4) },
-            new IndicatorChartDto { Value = 20, Date = new DateTime(2023, 1, 5) },
-            new IndicatorChartDto { Value = 20, Date = new DateTime(2023, 1, 5) }
-        };
+        var records = _chartServices.GetRangeAsync(1, 1, 
+            new DateTime(), new DateTime()).Result;
 
         var dataValues = records.Select(r => r.Value).ToList();
         var dateLabels = records.Select(r => r.Date).ToList();
