c6d55a8ad6d352cec0489f504f9800aa053f44ed
mk2b79 <samchuk.maks@outlook.com>
2024-01-03 13:49:59 +0200

    Add Test Data in ChartServices

--- GasReact.BLL/Services/ChartServices.cs
+++ GasReact.BLL/Services/ChartServices.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using AutoMapper;
 using GasReact.BLL.DTO;
 using GasReact.DAL.Entities;
 using GasReact.DAL.Repositories;
@@ -8,15 +9,44 @@ namespace GasReact.BLL.Services;
 public class ChartServices
 {
     private readonly IInicatorRepository _indicatorsRepository;
+    private readonly IMapper _mapper;
     
-    public ChartServices(IInicatorRepository indicatorsRepository)
+    public ChartServices(IInicatorRepository indicatorsRepository, IMapper mapper)
     {
         _indicatorsRepository = indicatorsRepository;
+        _mapper = mapper;
     }
 
-    public async Task<IEnumerable<IndicatorChartDto>> GetRangeAsync(FermentatorDto fermentator,
-        IndicatorTypeDto indicatorType, DateTime startDate, DateTime endDate)
+    public async Task<IEnumerable<IndicatorChartDto>> GetRangeAsync(int fermentatorId,
+        int indicatorTypeId, DateTime startDate, DateTime endDate)
     {
-        throw new NotImplementedException();
+        //Return test data
+        return testData();
+    }
+
+    private IEnumerable<IndicatorChartDto> testData()
+    {
+        List<IndicatorChartDto> records = new List<IndicatorChartDto>
+        {
+            new IndicatorChartDto { Value = 10, Date = new DateTime(2023, 1, 1) },
+            new IndicatorChartDto { Value = 25, Date = new DateTime(2023, 1, 2) },
+            new IndicatorChartDto { Value = 13, Date = new DateTime(2023, 1, 3) },
+            new IndicatorChartDto { Value = 32, Date = new DateTime(2023, 1, 4) },
+            new IndicatorChartDto { Value = 20, Date = new DateTime(2023, 1, 5) },
+            new IndicatorChartDto { Value = 10, Date = new DateTime(2023, 1, 1) },
+            new IndicatorChartDto { Value = 10, Date = new DateTime(2023, 1, 1) },
+            new IndicatorChartDto { Value = 25, Date = new DateTime(2023, 1, 2) },
+            new IndicatorChartDto { Value = 13, Date = new DateTime(2023, 1, 3) },
+            new IndicatorChartDto { Value = 32, Date = new DateTime(2023, 1, 4) },
+            new IndicatorChartDto { Value = 20, Date = new DateTime(2023, 1, 5) },
+            new IndicatorChartDto { Value = 10, Date = new DateTime(2023, 1, 1) },
+            new IndicatorChartDto { Value = 25, Date = new DateTime(2023, 1, 2) },
+            new IndicatorChartDto { Value = 13, Date = new DateTime(2023, 1, 3) },
+            new IndicatorChartDto { Value = 32, Date = new DateTime(2023, 1, 4) },
+            new IndicatorChartDto { Value = 20, Date = new DateTime(2023, 1, 5) },
+            new IndicatorChartDto { Value = 10, Date = new DateTime(2023, 1, 1) },
+            new IndicatorChartDto { Value = 25, Date = new DateTime(2023, 1, 2) }
+        };
+        return records;
     }
 }
