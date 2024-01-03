3e9adac03835936f616bce869c58482cad2f67df
mk2b79 <samchuk.maks@outlook.com>
2024-01-03 14:00:51 +0200

    Random test data

--- GasReact.BLL/Services/ChartServices.cs
+++ GasReact.BLL/Services/ChartServices.cs
@@ -26,26 +26,15 @@ public class ChartServices
 
     private IEnumerable<IndicatorChartDto> testData()
     {
+        Random random = new Random();
         List<IndicatorChartDto> records = new List<IndicatorChartDto>
         {
-            new IndicatorChartDto { Value = 10, Date = new DateTime(2023, 1, 1) },
-            new IndicatorChartDto { Value = 25, Date = new DateTime(2023, 1, 2) },
-            new IndicatorChartDto { Value = 13, Date = new DateTime(2023, 1, 3) },
-            new IndicatorChartDto { Value = 32, Date = new DateTime(2023, 1, 4) },
-            new IndicatorChartDto { Value = 20, Date = new DateTime(2023, 1, 5) },
-            new IndicatorChartDto { Value = 10, Date = new DateTime(2023, 1, 1) },
-            new IndicatorChartDto { Value = 10, Date = new DateTime(2023, 1, 1) },
-            new IndicatorChartDto { Value = 25, Date = new DateTime(2023, 1, 2) },
-            new IndicatorChartDto { Value = 13, Date = new DateTime(2023, 1, 3) },
-            new IndicatorChartDto { Value = 32, Date = new DateTime(2023, 1, 4) },
-            new IndicatorChartDto { Value = 20, Date = new DateTime(2023, 1, 5) },
-            new IndicatorChartDto { Value = 10, Date = new DateTime(2023, 1, 1) },
-            new IndicatorChartDto { Value = 25, Date = new DateTime(2023, 1, 2) },
-            new IndicatorChartDto { Value = 13, Date = new DateTime(2023, 1, 3) },
-            new IndicatorChartDto { Value = 32, Date = new DateTime(2023, 1, 4) },
-            new IndicatorChartDto { Value = 20, Date = new DateTime(2023, 1, 5) },
-            new IndicatorChartDto { Value = 10, Date = new DateTime(2023, 1, 1) },
-            new IndicatorChartDto { Value = 25, Date = new DateTime(2023, 1, 2) }
+            new IndicatorChartDto { Value = random.Next(0,100), Date = new DateTime(2023, 1, 1) },
+            new IndicatorChartDto { Value = random.Next(0,100), Date = new DateTime(2023, 1, 1) },
+            new IndicatorChartDto { Value = random.Next(0,100), Date = new DateTime(2023, 1, 1) },
+            new IndicatorChartDto { Value = random.Next(0,100), Date = new DateTime(2023, 1, 1) },
+            new IndicatorChartDto { Value = random.Next(0,100), Date = new DateTime(2023, 1, 1) },
+            new IndicatorChartDto { Value = random.Next(0,100), Date = new DateTime(2023, 1, 1) }
         };
         return records;
     }
